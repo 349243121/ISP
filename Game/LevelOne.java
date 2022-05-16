ff3c81fade9661b3d11ddc30b9674729a0f2efe5
estanzoo <mysupppermail@gmail.com>
2022-05-16 09:20:44 -0400

    written level interface

---
+++ Game/LevelOne.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class LevelOne implements Level
+{
+   public void changeLeftScreen()
+   {
+   
+   }
+   public void changeRightScreen()
+   {
+   
+   }
+   public void exit()
+   {
+   
+   }
+   public void background()
+   {
+   
+   }
+   public void dialogue()
+   {
+   
+   
+   }
+
+}
