b921069737f67da9c525f7a02a2aa79c71bc8bbf
estanzoo <mysupppermail@gmail.com>
2022-05-19 10:06:24 -0400

    fx test

---
+++ Gamefx/Test.java
@@ -0,0 +1,16 @@
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+
+public class Test extends Application {
+
+   public static void main (String [] args)
+   {
+      launch(args);
+   }
+   public void start (Stage stage) throws Exception {
+      //Stage stage = new Stage();
+      //Scene scene = new Scene();
+      stage.show();
+   }
+}
